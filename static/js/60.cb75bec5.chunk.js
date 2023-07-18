2dcd02fee58535c00590cc16a2c75f2862067134
Evgeniy-x <117513013+Evgeniy-x@users.noreply.github.com>
2023-07-18 10:38:00 +0000

    Deploying to gh-pages from @ Evgeniy-x/goit-react-hw-08-phonebook@874fccc1d660116f595c21bf328e1648a1cc4817 🚀

---
+++ static/js/60.cb75bec5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[60],{3060:function(e,n,o){o.r(n);var s=o(184);n.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h1",{style:{fontSize:60},children:"Welcome"}),(0,s.jsx)("h2",{children:"This is phonebook app for your contacts."}),(0,s.jsx)("p",{style:{fontSize:20},children:"Here you can log in and save the necessary numbers of people you need in your account."})]})}}}]);
+//# sourceMappingURL=60.cb75bec5.chunk.js.map
